c6c507273c227aed89de7a9265dec6509d5ab762
Andrei <bragari.andrei@carasent.com>
2024-06-16 16:29:14 +0300

    feat: Added scaffold UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+const mockUrls = [
+  "https://utfs.io/f/7bf6126a-b138-44f4-8822-2c1d29e4fcfd-rc8nbu.jpg",
+  "https://utfs.io/f/16141d02-1535-46cb-b290-2f65706dfde9-x51uau.jpg",
+  "https://utfs.io/f/b4f4609a-00b3-484a-92d9-34789d147b25-sg3q6.jpg",
+  "https://utfs.io/f/1860c7fa-910e-4188-a722-2af9c905fb3b-sg3qx.jpg",
+]
+
+const mockImages = mockUrls.map((url, i) => ({
+  id: i + 1,
+  url
+}))
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-     Home page
+    <main className="">
+      <div className="flex flex-wrap gap-4">{
+        mockImages.map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt={`image ${image.id}`} />
+          </div>
+        ))
+      }</div>
     </main>
   );
 }
