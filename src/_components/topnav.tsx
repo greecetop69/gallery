ed088b1534b460da445ada6b0fb08e72d446f14f
Andrei <bragari.andrei@carasent.com>
2024-06-24 22:43:41 +0300

    feat: Added uploadthing

--- src/_components/topnav.tsx
+++ src/_components/topnav.tsx
@@ -1,14 +1,21 @@
+"use client";
+
 import { SignedOut, UserButton, SignedIn, SignInButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { UploadButton } from "~/utils/uploadthing";
 
 export function TopNav() {
+  const Router = useRouter();
+
     return (
       <nav className="flex w-full items-center justify-between border-b text-xl font-semibold p-4">
         <div className="">Gallery</div>
-        <div className="">
+        <div className="flex flex-row">
             <SignedOut>
                 <SignInButton />
             </SignedOut>
             <SignedIn>
+              <UploadButton endpoint="imageUploader" onClientUploadComplete={ () => {Router.refresh();} } />
                 <UserButton />
             </SignedIn>
         </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import "~/styles/globals.css";
+import "@uploadthing/react/styles.css";
 
-import { GeistSans } from "geist/font/sans";
 import { ClerkProvider } from "@clerk/nextjs";
 import { TopNav } from "~/_components/topnav";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "./api/uploadthing/core";
 
 export const metadata = {
   title: "Create T3 Gallery",
@@ -13,7 +16,10 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
-      <html lang="en" className={`${GeistSans.variable}`}>
+      <html lang="en" >
+      <NextSSRPlugin
+          routerConfig={extractRouterConfig(ourFileRouter)}
+        />
         <body className="flex flex-col gap-4">
           <TopNav />
           {children}
