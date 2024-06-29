272f7f406234b51aaf276283162373bc4a43eee2
Andrei <bragari.andrei@carasent.com>
2024-06-29 15:08:18 +0300

    feat: Added start implemented parallel routes

--- src/server/queries.ts
+++ src/server/queries.ts
@@ -10,8 +10,22 @@ export async function getMyImages() {
 
 
     const images = await db.query.images.findMany({
-        where: (model, {eq} ) => eq(model.userId, user.userId),
+        where: (model, { eq }) => eq(model.userId, user.userId),
         orderBy: (model, { asc }) => asc(model.id),
     });
     return images;
+}
+
+export async function getImage(id: number) {
+    const user = auth();
+    if (!user.userId) throw new Error("Unauthorized");
+
+    const image = await db.query.images.findFirst({
+        where: (model, { eq }) => eq(model.id, id),
+    });
+    if (!image) throw new Error("Image not found");
+
+    if (image.userId !== user.userId) throw new Error("Unauthorized");
+
+    return image;
 }
