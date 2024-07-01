f6bd2a257c93e0374b20433383dc23a883bd543e
Andrei <bragari.andrei@carasent.com>
2024-07-01 07:59:25 +0300

    fixed: More cleanup in model

--- src/app/components/FullPageImageView.tsx
+++ src/app/components/FullPageImageView.tsx
@@ -2,5 +2,18 @@ import { getImage } from "~/server/queries";
 
 export default async function FullPageImageView(props: { id: number }) {
   const image = await getImage(props.id);
-  return <img src={image.url} alt={image.name} className="w-96" />;
+  return (
+    <div className="flex h-full w-full min-w-0">
+      <div className="flex-shrink-0 flex justify-center items-center">
+        <img
+          src={image.url}
+          alt={image.name}
+          className="flex-shrink-0 object-contain"
+        />
+      </div>
+      <div className="flex w-48 flex-col flex-shrink-0 border-l">
+        <div className="text-xl font-bold">{image.name} </div>
+      </div>
+    </div>
+  );
 }
