ba5e9c65efbc70bc80e3db62527ecd9cb0b6203a
Andrei <bragari.andrei@carasent.com>
2024-07-04 00:13:08 +0300

    fix: Fixed photo view

--- src/_components/topnav.tsx
+++ src/_components/topnav.tsx
@@ -4,20 +4,20 @@ import { SignedOut, UserButton, SignedIn, SignInButton } from "@clerk/nextjs";
 import { SimpleUploadButton } from "./simpleUploadButton";
 
 export function TopNav() {
-
-    return (
-      <nav className="flex w-full items-center justify-between border-b text-xl font-semibold p-4">
-        <div className="">Gallery</div>
-        <div className="flex flex-row">
-            <SignedOut>
-                <SignInButton />
-            </SignedOut>
-            <SignedIn>
-              <SimpleUploadButton />
-                <UserButton />
-            </SignedIn>
-        </div>
-      </nav>
-    );
-  }
-  
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
+      <div className="">Gallery</div>
+      <div className="flex flex-row">
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <div className="mr-4">
+            <SimpleUploadButton />
+          </div>
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  );
+}
--- src/app/components/FullPageImageView.tsx
+++ src/app/components/FullPageImageView.tsx
@@ -7,25 +7,17 @@ export default async function FullPageImageView(props: { id: number }) {
   const uploaderInfo = await clerkClient.users.getUser(image.userId);
 
   return (
-    <div className="flex h-full w-full min-w-0">
-      <div className="flex flex-shrink-0 items-center justify-center">
+    <div className="flex h-full w-full min-w-0 items-center justify-center">
+      <div className="flex justify-items-start">
         <img
           src={image.url}
           alt={image.name}
-          className="flex-shrink-0 object-contain"
+          className="object-contain"
         />
-      </div>
-      <div className="flex w-48 flex-shrink-0 flex-col border-l">
-        <div className="border-b border-r p-2 text-center text-lg">
-          {image.name}
-        </div>
-        <div className="flex flex-col p-2">
-          <span>Created on:</span>
-          <span>{new Date(image.createdAt).toLocaleDateString()}</span>
-        </div>
-        <div className="flex flex-col p-2">
-          <span>Uploaded by:</span>
-          <span>{uploaderInfo.username}</span>
+        <div className="bg-black bg-opacity-50 text-white p-2 text-left ml-4">
+          <div className="text-lg">{image.name}</div>
+          <div>Created on: {new Date(image.createdAt).toLocaleDateString()}</div>
+          <div>Uploaded by: {uploaderInfo.username}</div>
         </div>
       </div>
     </div>
