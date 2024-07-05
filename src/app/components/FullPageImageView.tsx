91ce7c648d09d6196eaa353a42c2911d4ac73e0c
greecetop69 <acme.ms12@mail.ru>
2024-07-05 11:39:53 +0300

    fix: fIxed prettier

--- src/app/components/FullPageImageView.tsx
+++ src/app/components/FullPageImageView.tsx
@@ -9,17 +9,17 @@ export default async function FullPageImageView(props: { id: number }) {
   return (
     <div className="flex h-full w-full min-w-0 items-center justify-center">
       <div className="flex justify-items-start  rounded">
-        <img
-          src={image.url}
-          alt={image.name}
-          className="object-contain"
-        />
-      <div className="relative text-white p-2 text-left pl-4 rounded-r-lg">
-  <div className="absolute inset-0 bg-zinc-700 opacity-75 rounded-r-lg"></div>
-  <div className="relative z-10 text-lg">{image.name}</div>
-  <div className="relative z-10">Created on: {new Date(image.createdAt).toLocaleDateString()}</div>
-  <div className="relative z-10">Uploaded by: {uploaderInfo.username}</div>
-</div>
+        <img src={image.url} alt={image.name} className="object-contain" />
+        <div className="relative rounded-r-lg p-2 pl-4 text-left text-white">
+          <div className="absolute inset-0 rounded-r-lg bg-zinc-700 opacity-75"></div>
+          <div className="relative z-10 text-lg">{image.name}</div>
+          <div className="relative z-10">
+            Created on: {new Date(image.createdAt).toLocaleDateString()}
+          </div>
+          <div className="relative z-10">
+            Uploaded by: {uploaderInfo.username}
+          </div>
+        </div>
       </div>
     </div>
   );
