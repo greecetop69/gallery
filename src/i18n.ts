9aa09716c26333a42f8bc2f947d608b932646e63
greecetop69 <acme.ms12@mail.ru>
2024-07-29 16:00:08 +0300

    fix: Fixed deploy

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,11 @@
 import { notFound } from 'next/navigation';
-import { getRequestConfig } from 'next-intl/server';
+import { getRequestConfig, unstable_setRequestLocale } from 'next-intl/server';
 
 const locales = ['en', 'ru'];
 
-export default getRequestConfig(async ({ locale }) => {
+export default getRequestConfig(async ({ req, locale }) => {
+    unstable_setRequestLocale(req, locale);
+
     if (!locales.includes(locale as any)) notFound();
 
     return {
